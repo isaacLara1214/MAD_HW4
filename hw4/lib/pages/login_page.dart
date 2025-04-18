64a1d9b4c890f905e58ff3de1edb754392d3b297
Isaac Lara <isaac.lara014@gmail.com>
2025-04-18 10:50:56 -0400

    set up firebase

---
+++ hw4/lib/pages/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 50,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ hw4/lib/theme/light_mode.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: Colors.blue,
+    secondary: Colors.blueAccent,
+    surface: Colors.white,
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
+    onSurface: Colors.black,
+  ),
+);
